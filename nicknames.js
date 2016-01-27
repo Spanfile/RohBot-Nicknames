151ba1c3f26059489498a62dd0dc674cb528f6f6
Santeri Kasvi <santeri.kasvi@gmail.com>
2016-01-27 13:23:00 +0200

    Added ability to nickname by id, added /whois

--- nicknames.js
+++ nicknames.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         RohBot Nicknames
-// @version      1.1
+// @version      1.2
 // @description  Allows the user set custom nicknames to other users
 // @author       Spans
 // @match        https://rohbot.net
@@ -11,7 +11,7 @@
 'use strict';
 
 var storeKey = "spans-nicknames";
-var nicknames = {};
+var nicknames = {}; // id: { nick, original }
 
 function loadNicks() {
 	nicknames = JSON.parse(RohStore.get(storeKey) || "{}");
@@ -25,27 +25,62 @@ function unescape(str) {
 	return $('<textarea/>').html(str).text();
 }
 
-cmd.register("nick", "-]", function (chat, args) {
-	if (args.length != 2 || args[0].length === 0 || args[1].length === 0) {
-		chat.statusMessage("Usage: /nick user nickname");
+cmd.register("nick", "--]", function (chat, args) {
+	if (args.length != 3 || args[0].length === 0 || args[1].length === 0 || args[2].length === 0) {
+		chat.statusMessage("Usage: /nick (id|name) nickname user");
 		return;
 	}
 	
-	var user = chat.userList.find(function (user) { return unescape(user.Name) == args[0]; });
+	var user;
+	if (args[0] == "id") {
+		user = chat.userList.find(function (user) { return user.UserId == args[2]; });
+	} else if (args[0] == "name") {
+		user = chat.userList.find(function (user) { return unescape(user.Name) == args[2]; });
+	}
+	
 	if (!user) {
-		chat.statusMessage("Who's " + args[0] + "?");
+		chat.statusMessage("Who's " + args[2] + "?");
 		return;
 	}
 	
-	nicknames[user.UserId] = args[1];
-	chat.statusMessage(args[0] + " is now " + args[1] + "!");
+	nicknames[user.UserId] = { nick: args[1], original: user.Name };
+	chat.statusMessage(user.Name + " is now " + args[1] + "!");
 	
 	saveNicks();
 });
 
-cmd.register("removenick", "-", function (chat, args) {
+cmd.register("removenick", "-]", function (chat, args) {
+	if (args.length != 2 || args[0].length === 0 || args[0].length === 0) {
+		chat.statusMessage("Usage: /removenick (id|name) nickname|id");
+		return;
+	}
+	
+	var user;
+	if (args[0] == "id") {
+		user = chat.userList.find(function (user) { return user.UserId == args[1]; });
+	} else if (args[0] == "name") {
+		user = chat.userList.find(function (user) { return unescape(user.Name) == args[1]; });
+	}
+	
+	if (!user) {
+		chat.statusMessage("Who's " + args[1] + "?");
+		return;
+	}
+	
+	if (!nicknames[user.UserId]) {
+		chat.statusMessage(user.Name + " doesn't have a nickname :(");
+		return;
+	}
+	
+	chat.statusMessage(user.Name + " is just now " + nicknames[user.UserId].original + " :(");
+	delete nicknames[user.UserId];
+	
+	saveNicks();
+});
+
+cmd.register("whois", "]", function (chat, args) {
 	if (args.length != 1 || args[0].length === 0) {
-		chat.statusMessage("Usage: /removenick nickname");
+		chat.statusMessage("Usage: /whois nickname");
 		return;
 	}
 	
@@ -55,10 +90,13 @@ cmd.register("removenick", "-", function (chat, args) {
 		return;
 	}
 	
-	delete nicknames[user.UserId];
-	chat.statusMessage(args[0] + " ain't " + args[0] + " anymore :(");
+	var nick = nicknames[user.UserId];
+	if (!nick) {
+		chat.statusMessage(args[0] + " is just " + args[0] + " :(");
+		return;
+	}
 	
-	saveNicks();
+	chat.statusMessage("That's " + nick.original + "!");
 });
 
 chatMgr.lineFilter.add(function (line, prepend, e) {
@@ -66,20 +104,20 @@ chatMgr.lineFilter.add(function (line, prepend, e) {
 		var nick = nicknames[line.SenderId];
 		
 		if (nick) {
-			line.Sender = nick;
+			line.Sender = nick.nick;
 		}
 	} else {
 		var nick = nicknames[line.ForId];
 		
 		if (nick) {
-			line.For = nick;
+			line.For = nick.nick;
 		}
 		
 		if (line.By) {
 			var nick = nicknames[line.ById];
 			
 			if (nick) {
-				line.By = nick;
+				line.By = nick.nick;
 			}
 		}
 	}
@@ -89,7 +127,7 @@ chatMgr.userFilter.add(function (user, e) {
 	var nick = nicknames[user.UserId];
 	
 	if (nick) {
-		user.Name = nick;
+		user.Name = nick.nick;
 	}
 });
 
